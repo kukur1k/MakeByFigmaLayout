15329db0c802c0f5ecacab989a173ca0cba9b082
kukur1k <apurkin97@gmail.com>
2026-03-01 12:10:58 +0300

    make swiper

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//============================Swiper===================================
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  centeredSlides: true,
+  initialSlide: 1,
+
+
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+
+});
